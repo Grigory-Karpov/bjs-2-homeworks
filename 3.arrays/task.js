ee30d4649c9539571c8b273fa98c86b1c3577a69
Grigory Karpov <gregkarpov64@gmail.com>
2026-02-07 01:24:43 +0400

    Решение задач по массивам

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,21 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  return arr1.every((element, index) => element === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const filteredUsers = users.filter(user => user.gender === gender);
+
+  if (filteredUsers.length === 0) {
+    return 0;
+  }
+
+  const totalAge = filteredUsers
+    .map(user => user.age)
+    .reduce((acc, age) => acc + age, 0);
+
+  return totalAge / filteredUsers.length;
 }
