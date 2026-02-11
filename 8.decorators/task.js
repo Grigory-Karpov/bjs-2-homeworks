c9eea6f729a4f18fca59cd8bd1e495b5d180c761
Grigory Karpov <gregkarpov64@gmail.com>
2026-02-11 21:12:44 +0400

    Update task.js

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,54 @@
-//Задача № 1
+/* Задача 1. Усовершенствовать кеширующий декоратор */
+
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = md5(args);
+    let objectInCache = cache.find((item) => item.hash === hash);
+
+    if (objectInCache) {
+      console.log('Из кеша: ' + objectInCache.value);
+      return 'Из кеша: ' + objectInCache.value;
+    }
+
+    let result = func(...args);
+    cache.push({ hash, value: result });
+
+    if (cache.length > 5) {
+      cache.shift();
+    }
+
+    console.log('Вычисляем: ' + result);
+    return 'Вычисляем: ' + result;
+  }
+
+  return wrapper;
 }
 
-//Задача № 2
+/* Задача 2. Декоратор debounce с моментальным вызовом */
+
 function debounceDecoratorNew(func, delay) {
-  
+  let timeoutId = null;
+
+  function wrapper(...args) {
+    wrapper.allCount++;
+
+    if (timeoutId === null) {
+      func(...args);
+      wrapper.count++;
+    }
+
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => {
+      func(...args);
+      wrapper.count++;
+    }, delay);
+  }
+
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+
+  return wrapper;
 }
